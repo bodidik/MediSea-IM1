ba11b3b5a22f3d1a10203ca53a855128f0bb282e
Dz <you@example.com>
2025-10-03 12:05:52 +0300

    sections: tüm bölümler ortak şablona geçirildi (FS tarama + curated destek)

--- web/app/topics/romatoloji/behcet/page.tsx
+++ web/app/topics/romatoloji/behcet/page.tsx
@@ -3,7 +3,8 @@ export const dynamic = "force-dynamic";
 
 import Link from "next/link";
 
-const PAGES = [{"href":"/topics/romatoloji/behcet/behcet-disbiyoz","title":"Behçet hastalığı ve disbiyoz ilişkisi"},{"href":"/topics/romatoloji/behcet/behcet-mikroorganizmalar","title":"BH ve mikroorganizmalar (tetikleyiciler)"},{"href":"/topics/romatoloji/behcet/mikrobiyota-modulasyon","title":"Mikrobiyota modülasyonu: probiyotik/prebiyotik"},{"href":"/topics/romatoloji/behcet/oral-ulserler","title":"Oral ülserler: patogenez ve yönetim"},{"href":"/topics/romatoloji/behcet/uveit","title":"Göz tutulumu (üveit): tanı ve tedavi"},{"href":"/topics/romatoloji/behcet/vaskuler-tutulum","title":"Vasküler tutulum: tromboz/anevrizma"},{"href":"/topics/romatoloji/behcet/neuro-behcet","title":"Nöro-Behçet: klinik spektrum ve görüntüleme"},{"href":"/topics/romatoloji/behcet/mukokutanoz","title":"Muko-kutanöz bulgular ve algoritma"},{"href":"/topics/romatoloji/behcet/gebelik","title":"Gebelik ve Behçet: ilaç güvenliği/izlem"},{"href":"/topics/romatoloji/behcet/tedavi","title":"Tedavi: kolşisin, anti-TNF, apremilast, yeni ajanlar"}];
+const PAGES = [{"href":"/topics/romatoloji/behcet/behcet-disbiyoz","title":"Behçet hastalığı ve disbiyoz ilişkisi"},{"href":"/topics/romatoloji/behcet/behcet-mikroorganizmalar","title":"BH ve mikroorganizmalar (tetikleyiciler)"},{"href":"/topics/romatoloji/behcet/mikrobiyota-modulasyon","title":"Mikrobiyota modülasyonu: probiyotik/prebiyotik"},{"href":"/topics/romatoloji/behcet/oral-ulserler","title":"Oral ülserler: patogenez ve yönetim"},{"href":"/topics/romatoloji/behcet/uveit","title":"Göz tutulumu (üveit): tanı ve tedavi"},{"href":"/topics/romatoloji/behcet/vaskuler-tutulum","title":"Vasküler tutulum: tromboz/anevrizma"},{"href":"/topics/romatoloji/behcet/neuro-behcet","title":"Nöro-Behçet: klinik spektrum ve görüntüleme"},{"href":"/topics/romatoloji/behcet/mukokutanoz","title":"Muko-kutanöz bulgular ve algoritma"},{"href":"/topics/romatoloji/behcet/gebelik","title":"Gebelik ve Behçet: ilaç güvenliği/izlem"},{"href":"/topics/romatoloji/behcet/tedavi","title":"Tedavi: kolşisin, anti-TNF, apremilast, yeni ajanlar"}  , { slug: "gastroenterolojik-tutulum", title: "Gastroenterolojik Tutulum" }
+
 
 export default function BehcetIndex() {
   return (
@@ -23,5 +24,6 @@ export default function BehcetIndex() {
     </div>
   );
 }
+
 
 
